1580886a36d52f1f7f2f3f2a003a5cb593b4177f
hitxav <35729828+hitxav@users.noreply.github.com>
2020-11-08 00:30:39 +0530

    Add files via upload

---
+++ Hitesh Bani-Weekly Report/Week 3/adminlogin.php
@@ -0,0 +1,55 @@
+/* Add a Cascading Style sheet for designing the
+web page */
+
+/*External css is used */
+
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+ <link rel="stylesheet" 
+          href= 
+"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+	<title>
+		
+	</title>
+  <style type="text/css">
+    body
+ {  background-image: url("pic.png");
+ 
+    
+  background-repeat:no-repeat;
+  background-size: cover;
+}
+
+  </style>
+</head>
+<body>
+<div class="box" >
+<form action="check2.php" method="POST">
+	
+	<div class="in-container">
+	 <i class="fa fa-user icon"></i> 
+	<input type="text" id="name" name="name1" placeholder="Name" class="in" onblur="na()" >
+  </div>
+    <br>
+    
+    <div class="in-container">
+    <i class="fa fa-key icon"></i>
+    <input type="password" placeholder="Password" name="password1" id="pw" class="in" onblur="pass()">
+    </div> 
+    <br>
+   <br>
+   <input type="submit" value="LOGIN"  class="ppp"/>
+   <br><br>
+   
+   <a href="database.php">Create data base</a>
+</form>
+</div>
+<br><br>
+
+<script type="text/javascript" src="brain2.js"></script>
+</body>
+</html>
---
+++ Hitesh Bani-Weekly Report/Week 4/brain2.js
@@ -0,0 +1,23 @@
+/*Design a dynamic web page with validation using
+JavaScript*/
+
+function na()
+{
+ var name;
+ name=document.getElementById("name").value;
+ if(name==null || name=="")
+ 	alert("Please write your username");
+ else return true;
+
+}
+
+function pass()
+{
+var Password;
+Password=document.getElementById("pw").value;
+if(Password==null || Password=="")
+	alert("Please write your Password");
+else return true;
+}
+
+
